7a593577883be9c85887176437127b3f36267999
SpikyShade <103762012+spikyshade@users.noreply.github.com>
2023-01-23 17:10:11 +0000

    basic directory structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/nav'
 import Bio from './components/bio'
+import Footer from './components/footer'
 
 function App() {
     return (
         <>
             <NavBar />
             <Bio />
+            <Footer />
         </>
     );
 }
